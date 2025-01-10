66dd07be1080a5a8add24c927017cf5293e5015f
citRa <citrahop@protonmail.com>
2025-01-10 12:25:03 -0400

    feat(util/blips): Rework

--- modules/util/client.lua
+++ modules/util/client.lua
@@ -1,4 +1,4 @@
----@class Blip
+---@class Blip : OxClass
 ---@field coords table
 ---@field entity integer
 ---@field netId integer
@@ -10,10 +10,14 @@
 ---@field alpha number
 ---@field flash boolean
 ---@field flashtime integer
+---@field pulse boolean
 ---@field label string
 ---@field scale number
 ---@field shortRange boolean
-local Blip = {}
+---@field showHeading boolean
+---@field number number
+---@field category integer?
+local Blip = lib.class('Blip')
 
 ---Spawn blip
 function Blip:create()
@@ -27,6 +31,9 @@ function Blip:create()
         end
     elseif self.entity then
         blip = AddBlipForEntity(self.entity)
+        if self.showHeading then
+            ShowHeadingIndicatorOnBlip(blip, true)
+        end
     elseif self.netId then
         self.active = true
         self:entityWatcher()
@@ -34,7 +41,19 @@ function Blip:create()
     else
         return
     end
+    if self.showHeading and (self.coords?.w or self.entity) then
+        ShowHeadingIndicatorOnBlip(blip, true)
+        if not self.entity then
+            SetBlipRotation(blip, self.coords.w)
+        end
+    end
     self.active = true
+    self._blip = blip
+    self:update()
+end
+
+function Blip:update()
+    local blip = self._blip
     if self.sprite then SetBlipSprite(blip, self.sprite) end
     if self.colour then SetBlipColour(blip, self.colour) end
     if self.display then SetBlipDisplay(blip, self.display) end
@@ -42,11 +61,13 @@ function Blip:create()
     if self.shortRange then SetBlipAsShortRange(blip, true) end
     SetBlipFlashes(blip, self.flash)
     if self.flash then SetBlipFlashInterval(blip, self.flashtime or 1000) end
+    if self.pulse then PulseBlip(blip) end
+    if self.category then SetBlipCategory(blip, self.category) end
+    if self.number then ShowNumberOnBlip(blip, self.number) end
     SetBlipScale(blip, self.scale or 1.0)
     BeginTextCommandSetBlipName("STRING")
     AddTextComponentSubstringPlayerName(self.label)
     EndTextCommandSetBlipName(blip)
-    self._blip = blip
 end
 
 ---Despawn blip
@@ -63,13 +84,26 @@ function Blip:updateCoords(coords)
     if not self._blip then return end
     self.coords = coords
     SetBlipCoords(self._blip, coords.x, coords.y, coords.z)
+    if coords.w then SetBlipRotation(self._blip, math.ceil(coords.w)) end
+end
+
+function Blip:updateSprite(sprite)
+    if not self._blip then return end
+    self.sprite = sprite
+    self:update()
+end
+
+function Blip:updateColour(colour)
+    if not self._blip then return end
+    self.colour = colour
+    self:update()
 end
 
 ---Toggle flash state of blip
 function Blip:toggleFlash()
     if not self._blip then return end
     self.flash = not self.flash
-    SetBlipFlashes(self._blip, self.flash)
+    self:update()
 end
 
 ---Update blip lable
@@ -77,9 +111,7 @@ end
 function Blip:setLabel(label)
     if not self._blip then return end
     self.label = label
-    BeginTextCommandSetBlipName(self.label)
-    AddTextComponentSubstringBlipName(self._blip)
-    EndTextCommandSetBlipName(self._blip)
+    self:update()
 end
 
 ---Enable route to blip
@@ -120,31 +152,28 @@ function Blip:entityWatcher()
     end)
 end
 
-setmetatable(Blip, {
-    __index = function(self, key)
-        return rawget(self, key)
-    end,
-    __call = function(self, data)
-        data = data or {}
-        self.label = data.label
-        self.entity = data.entity
-        self.netId = data.netId
-        self.active = true
-        self.coords = data.coords
-        self.radius = data.radius
-        self.sprite = data.sprite
-        self.colour = data.colour or data.color
-        self.display = data.display
-        self.alpha = data.alpha
-        self.flash = data.flash or false
-        self.shortRange = data.shortRange or false
-        self.flashtime = data.flashtime
-        self.scale = data.scale
-        self:create()
-        return self
-    end,
-})
-
 return {
-    blip = Blip,
+    blip = function(data)
+        local blip = Blip:new()
+        blip.label = data.label
+        blip.entity = data.entity
+        blip.netId = data.netId
+        blip.active = true
+        blip.coords = data.coords
+        blip.radius = data.radius
+        blip.sprite = data.sprite
+        blip.colour = data.colour or data.color
+        blip.display = data.display
+        blip.alpha = data.alpha
+        blip.flash = data.flash or false
+        blip.pulse = data.pulse or false
+        blip.shortRange = data.shortRange or false
+        blip.flashtime = data.flashtime
+        blip.scale = data.scale
+        blip.showHeading = data.heading or data.showHeading or false
+        blip.number = data.number
+        blip.category = data.category
+        blip:create()
+        return blip
+    end,
 }
